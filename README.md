1eabbc220bf00d700a87795a56a09414c7385f99
Laurence Scotford <laurencescotford@gmail.com>
2021-01-31 16:21:18 +0000

    Add AI player

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ A responsive version of Tic Tac Toe (AKA Noughts and Crosses) with optional AI p
 
 This is a work in progress project
 
-Open in a browser to start a game. Currently two human players can play against each other. To play, take turns to enter the number of the cell where you want to make your mark.
+Open in a browser to start a game. The game can be played by two human players, one human and one computer player or two computer players. The computer player has five strengths. To play with another person, take turns to enter the number of the cell where you want to make your mark. To play with a computer, you enter the number of the cell to make your mark, then the computer will play its move immediately.
 
 The game will end when a win or draw condition is detected. At that point a new game can be started.
 
 TO DO:
 
-- Add in AI player
-- Pretty up the user interface
+- Create a stronger AI player (add look ahead functionality)
+- Pretty up the user interface and enable moves to be entered by clicking a cell
--- js/tictactoe.js
+++ js/tictactoe.js
@@ -13,8 +13,12 @@ var tictactoe = {
     [3, 4, 5],
     [6, 7, 8]
   ],
-  turn: 'O',  // A character representing the current player
+  AIThresholds: [100, 50, 10, 3, 0],
+  playerChars: ['O', 'X'],  // The characters representing the two players
+  turn: 0,  // A character representing the current player
   moves: 0,   // Number of moves made in the game
+  players: ['H', 'H'],  // Holds the player for O and X respectively ('H' = human, 'C' = computer)
+  AIStrengths: [0, 0],  // Holds the AI strengths for O and X respectively
 
   // Initialise the game on start up (set up UI handlers and begin new game)
   initialiseGame: function() {
@@ -23,7 +27,6 @@ var tictactoe = {
     });
     document.querySelector('#new').addEventListener('click', this.resetGame);
     document.querySelector('#play').addEventListener('click', this.play);
-    this.resetGame();
   },
 
   // Draw the current board based on the contents of the board array
@@ -31,7 +34,78 @@ var tictactoe = {
     for(let i = 0; i < 9; i++) {
       document.querySelector('#cell' + i).innerHTML = this.board[i];
     }
-    document.querySelector('#turn').innerHTML = this.turn;
+    document.querySelector('#turn').innerHTML = this.playerChars[this.turn];
+  },
+
+  // Gets the next move from a computer or human player
+  getMove: function() {
+    // If the current player is a computer player, make the next move
+    if (this.players[this.turn] == 'C') {
+      this.AIMove();
+    } else {
+      // Otherwise, enable the input controls for a human player
+      document.querySelector('#play').disabled = false;
+      document.querySelector('#quit').disabled = false;
+    }
+  },
+
+  // Calculate the AI player's move
+  AIMove: function() {
+    // Create empty array for scores
+    let scores = [];
+
+    // Set the maximum score to 0
+    let maxScore = 0;
+
+    // Loop through the locations and generate a score for each
+    for (let i = 0; i < 9; i++) {
+      // If this space on the board is occupied ...
+      if (this.board[i] != ' ') {
+        // Give it a negative score
+        scores[i] = -1;
+      } else {
+        // Calculate a score for this location
+        // Set intial value to 0
+        scores[i] = 0;
+
+        // Find each line that contains the current position
+        for (let j = 0; j < this.lines.length; j++) {
+          if (this.lines[j].includes(i)) {
+            // Count number of marks in line made by AI player and other player
+            let ownMarks = this.countMarks(this.turn, j);
+            let opponentMarks = this.countMarks(this.otherPlayer(), j);
+            if (ownMarks == 0) {
+              if (opponentMarks == 0) {
+                scores[i]++;
+              } else {
+                scores[i] += opponentMarks == 2 ? 10 : 1;
+              }
+            } else if (opponentMarks == 0) {
+              scores[i] += ownMarks == 2 ? 20 : 2;
+            }
+          }
+        }
+
+        // Adjust value based on AI threshold (strength)
+        if (scores [i] <= this.AIThresholds[this.AIStrengths[this.turn]]) {
+          scores[i] = 0;
+        }
+
+        // Update max score if necessary
+        maxScore = Math.max(maxScore, scores[i]);
+      }
+    }
+
+    // Collect all the locations that have the maximum score
+    let possibleMoves = [];
+    for (let i = 0; i < scores.length; i++) {
+      if (scores[i] == maxScore) {
+        possibleMoves.push(i);
+      }
+    }
+
+    // Select a move at random from those available
+    this.processMove(possibleMoves[Math.floor(Math.random() * possibleMoves.length)]);
   },
 
   // Called when a player inputs a move. Validates the move, then updates and
@@ -43,6 +117,8 @@ var tictactoe = {
 
     valWarning.classList.add('hidden');
     posWarning.classList.add('hidden');
+    document.querySelector('#play').disabled = true;
+    document.querySelector('#quit').disabled = true;
 
     let selectedCell = parseInt(cellInput.value);
 
@@ -58,17 +134,7 @@ var tictactoe = {
       } else {
         // if valid cell has been entered, process the move
         cellInput.value = '';
-
-        tictactoe.board[selectedCell] = tictactoe.turn;
-        tictactoe.turn = tictactoe.otherPlayer();
-              tictactoe.drawBoard();
-
-        // Check for a won game, then for a draw
-        if (tictactoe.checkForWin()) {
-          tictactoe.quit(true);
-        } else if (++tictactoe.moves == 9) {
-          tictactoe.quit(false);
-        }
+        tictactoe.processMove(selectedCell);
       }
     }
   },
@@ -76,7 +142,7 @@ var tictactoe = {
   // Checks if either player has achieved a line of three marks
   checkForWin: function() {
     for (let i = 0; i < this.lines.length; i++) {
-      if (this.countMarks('O', i) == 3 || this.countMarks('X', i) == 3) {
+      if (this.countMarks(0, i) == 3 || this.countMarks(1, i) == 3) {
         return true;
       }
     }
@@ -87,11 +153,28 @@ var tictactoe = {
   countMarks: function(piece, line) {
     let count = 0;
     for (let i = 0; i < this.lines[line].length; i++) {
-      if (this.board[this.lines[line][i]] == piece) {
+      if (this.board[this.lines[line][i]] == this.playerChars[piece]) {
         count++;
       }
     }
     return count;
+  },
+
+  // Process a move for the current player
+  processMove: function(selectedCell) {
+    this.board[selectedCell] = tictactoe.playerChars[tictactoe.turn];
+    this.turn = tictactoe.otherPlayer();
+    this.drawBoard();
+
+    // Check for a won game, then for a draw
+    if (this.checkForWin()) {
+      this.quit(true);
+    } else if (++this.moves == 9) {
+      this.quit(false);
+    } else {
+      // If neither, get next move
+      this.getMove();
+    }
   },
 
   // Called when the game ends. Displays the winner or a draw message and the
@@ -103,29 +186,36 @@ var tictactoe = {
     drawMsg.classList.add('hidden');
 
     if(gameIsWon) {
-      document.querySelector('#winner').innerHTML = this.otherPlayer();
+      document.querySelector('#winner').innerHTML = this.playerChars[this.otherPlayer()];
       winMsg.classList.remove('hidden');
     } else {
       drawMsg.classList.remove('hidden');
     }
 
     document.querySelector('#inPlayControls').classList.add('hidden');
-    document.querySelector('#outOfPlayControls').classList.remove('hidden');
+    document.querySelector('#resultMsg').classList.remove('hidden');
+    document.querySelector('#newGameControls').classList.remove('hidden');
   },
 
   // Starts a new game, clearing the board, setting the first player to 'O' and
   // showing the UI for entering moves
   resetGame: function() {
     tictactoe.board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
-    tictactoe.turn = 'O';
+    tictactoe.turn = 0;
     tictactoe.moves = 0;
+    tictactoe.players[0] = document.querySelector('#playerOComputer').checked ? 'C' : 'H';
+    tictactoe.players[1] = document.querySelector('#playerXComputer').checked ? 'C' : 'H';
+    tictactoe.AIStrengths[0] = parseInt(document.querySelector('#compStrengthO').value);
+    tictactoe.AIStrengths[1] = parseInt(document.querySelector('#compStrengthX').value);
     document.querySelector('#inPlayControls').classList.remove('hidden');
-    document.querySelector('#outOfPlayControls').classList.add('hidden');
+    document.querySelector('#resultMsg').classList.add('hidden');
+    document.querySelector('#newGameControls').classList.add('hidden');
     tictactoe.drawBoard();
+    tictactoe.getMove();
   },
 
-  // Returns the character of the player not currently in play
+  // Returns the index number of the player not currently in play
   otherPlayer: function() {
-    return tictactoe.turn == 'O' ? 'X' : 'O';
+    return 1 - tictactoe.turn;
   }
 }
